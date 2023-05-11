30e3aaa04ced1c51ed34a6f957298d45834c3112
Emerson <emerzon.lc@gmail.com>
2023-05-11 10:31:57 -0500

    fix:active-slider

--- index.js
+++ index.js
@@ -1,45 +1,8 @@
-const nameInput = document.querySelector("#name");
-const emailInput = document.querySelector("#email");
-const descripcionInput = document.querySelector("#descripcion");
-const buttonEnviarEl = document.querySelector("#buttonEnviar");
-
-let nameValue = "";
-nameInput.addEventListener("input", (event) => {
-  nameValue = event.target.value;
-});
-
-let emailValue = "";
-emailInput.addEventListener("input", (event) => {
-  emailValue = event.target.value;
-});
-
-let descripcionValue;
-descripcionInput.addEventListener("input", (event) => {
-  descripcionValue = event.target.value;
-});
-
-const validateCharacters = ["@", ".", "com"];
-
-buttonEnviarEl.addEventListener("click", (click) => {
-  const nameError = document.querySelector("#nameError");
-  nameError.textContent = nameValue ? "" : "name required";
-
-  const emailError = document.querySelector("#emailError");
-  const isEmailOk = validateCharacters.every((character) =>
-    emailValue.includes(character)
-  );
-  emailError.textContent = emailValue && isEmailOk ? "" : "email required";
-
-  const descripcionError = document.querySelector("#descripcionError");
-  descripcionError.textContent = descripcionValue ? "" : "description required";
-});
-
+//slider
 const sliderActives1 = document.querySelectorAll("#sliderActive1");
 const sliderActives2 = document.querySelectorAll("#sliderActive2");
 const sliderActives3 = document.querySelectorAll("#sliderActive3");
 const cakesChange = document.querySelector("#cakesChange");
-
-console.log(sliderActives1);
 
 sliderActives1.forEach((sliderActive1) => {
   sliderActive1.addEventListener("click", () => {
@@ -80,12 +43,39 @@ sliderActives3.forEach((sliderActive3) => {
   });
 });
 
-// sliderActive2.onclick = function () {
-//   cakesChange.src = "./images/cakeslider2.svg";
-// };
-// sliderActive3.onclick = function () {
-//   cakesChange.src = "./images/cakeslider3.svg";
-// };
+//formlario
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const descripcionInput = document.querySelector("#descripcion");
+const buttonEnviarEl = document.querySelector("#buttonEnviar");
 
-// const sliderButtons = document.querySelectorAll(".slider__circle");
-// const changeLogos = document.querySelectorAll(".logoImage");
+let nameValue = "";
+nameInput.addEventListener("input", (event) => {
+  nameValue = event.target.value;
+});
+
+let emailValue = "";
+emailInput.addEventListener("input", (event) => {
+  emailValue = event.target.value;
+});
+
+let descripcionValue;
+descripcionInput.addEventListener("input", (event) => {
+  descripcionValue = event.target.value;
+});
+
+const validateCharacters = ["@", ".", "com"];
+
+buttonEnviarEl.addEventListener("click", (click) => {
+  const nameError = document.querySelector("#nameError");
+  nameError.textContent = nameValue ? "" : "name required";
+
+  const emailError = document.querySelector("#emailError");
+  const isEmailOk = validateCharacters.every((character) =>
+    emailValue.includes(character)
+  );
+  emailError.textContent = emailValue && isEmailOk ? "" : "email required";
+
+  const descripcionError = document.querySelector("#descripcionError");
+  descripcionError.textContent = descripcionValue ? "" : "description required";
+});
